8950e4418a1226614042a220a27c057b0e032c9d
mario-mitchell <mario.mitchell8291@gmail.com>
2019-01-04 00:48:07 -0800

    making for changes on how you can win.

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 let boxes = document.querySelectorAll(".row > div");
 let player = "X";
+let winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+]
 
 boxes.forEach(function(box){
     box.addEventListener("click", clickedBox);
 });
+
+newGame();
+
+function newGame(){
+    document.querySelector("restartGame").style.display = "none"
+    
+}
 
 function clickedBox(e){
     if(e.target.textContent !== ""){
